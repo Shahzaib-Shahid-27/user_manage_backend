92bbc57486d6fddef9380f32a004364f6abe7f52
Shahzaib Shahid <shahzaibshahidr10@gami.com>
2026-09-09 19:05:17 +0500

    railwey fix

--- src/server.ts
+++ src/server.ts
@@ -13,12 +13,13 @@ const port = Number(process.env.PORT) || 3000;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use(cors({
-    origin:process.env.FRONTEND_URL,
-    credentials:true,
-methods:["POST","GET","DELETE","PUT","OPTIONS"],
-}));
+// app.use(cors({
+//     origin:process.env.FRONTEND_URL,
+//     credentials:true,
+// methods:["POST","GET","DELETE","PUT","OPTIONS"],
+// }));
 
+app.use(cors());
 
 // router
 app.use( "/user" , my_router) ;
